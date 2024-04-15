27cc5b5caf22f322e42c82a61b136f6a29c9bf8a
Dante4rt <rxmxdhxni@gmail.com>
2024-04-15 15:13:56 +0700

    fix: error flow part 2

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+
 if ! command -v solana &> /dev/null
 then
     echo "Solana is not installed. Installing..."
-    sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)"
-    echo "Solana has been installed."
-    echo "Adding Solana to PATH..."
 
-    install_output=$(sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)")
-    solana_path=$(echo "$install_output" | grep -oP '(?<=export PATH=")[^:]+')
+    install_logs=$(sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)" 2>&1)
+    echo "Solana has been installed."
+
+    solana_path=$(echo "$install_logs" | grep -oP 'export PATH="\K[^"]+(?=" to)')
+    echo "Adding Solana to PATH..."
 
     export PATH="$solana_path:$PATH"
     echo "Solana has been added to the PATH."
@@ -16,8 +17,10 @@ else
     echo "Solana is already installed."
 fi
 
+
 solana_version=$(solana --version)
 echo "Solana version: $solana_version"
+
 
 echo "Enter the character you want to use for generating the key (max 8 characters): "
 read -r char_name
@@ -28,10 +31,14 @@ read -r choice
 
 case $choice in
     1)
-        solana-keygen grind --starts-with "$char_name":1
+        echo "Enter the number of threads (1-16): "
+        read -r threads
+        solana-keygen grind --starts-with "$char_name":$threads
         ;;
     2)
-        solana-keygen grind --ends-with "$char_name":1
+        echo "Enter the number of threads (1-16): "
+        read -r threads
+        solana-keygen grind --ends-with "$char_name":$threads
         ;;
     *)
         echo "Invalid choice. Please enter either 1 or 2."
