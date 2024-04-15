7669efd8c1de9f44b7e3bfbbd32c17f3b7b636f7
Dante4rt <rxmxdhxni@gmail.com>
2024-04-15 15:20:19 +0700

    feat: enhance path

--- run.sh
+++ run.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
+echo "Showing HCA logo..."
+wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
+curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
+sleep 2
 
 if ! command -v solana &> /dev/null
 then
     echo "Solana is not installed. Installing..."
-
     install_logs=$(sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)" 2>&1)
     echo "Solana has been installed."
-
-    solana_path=$(echo "$install_logs" | grep -oP 'export PATH="\K[^"]+(?=" to)')
-    echo "Adding Solana to PATH... $solana_path"
-    
+    solana_path=$(echo "$install_logs" | grep -oP 'PATH="\K[^"]+(?=")')
+    echo "Adding Solana to PATH..."
     export PATH="$solana_path:$PATH"
     echo "Solana has been added to the PATH."
 else
     echo "Solana is already installed."
 fi
 
-
 solana_version=$(solana --version)
 echo "Solana version: $solana_version"
-
 
 echo "Enter the character you want to use for generating the key (max 8 characters): "
 read -r char_name
